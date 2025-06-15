82600adcc0b08c7fc5d9e552f389f05059ac979c
MattijsVandebroekPXL <mattijs.vandebroek@student.pxl.be>
2025-06-15 10:33:01 +0200

    Update timer.js
    geprobeerd valsspelerij te voorkomen

--- website/overige/timer/assets/scripts/timer.js
+++ website/overige/timer/assets/scripts/timer.js
@@ -4,6 +4,7 @@ let firstClickTime = null;
 let highscore = null;
 let amountOfTries = 0;
 let total = 0;
+let modus = "none";
 
 const handleLoad = () =>
 {
@@ -13,92 +14,120 @@ const handleLoad = () =>
 
 const handleClick = () =>
 {
-    document.removeEventListener('keydown', handleKeyDown);
-    document.removeEventListener('keyup', handleKeyUp);
-
-    const currentTime = new Date().getTime(); // Get current time in milliseconds
-
-    if (firstClickTime === null)
+    if(modus === "none")
     {
-        firstClickTime = currentTime; // Record the time of the first click
-        console.log('First click recorded. Click again to measure the time span.');
-        document.getElementById('timertijd').innerText = "Klik opnieuw om de timer te stoppen";
+        modus = "click";
     }
-    else
+    if(modus === "click")
     {
-        const timeSpan = currentTime - firstClickTime; // Calculate the time difference
-        console.log(`Time span between clicks: ${timeSpan} ms`);
-        firstClickTime = null; // Reset for the next measurement
-        document.addEventListener('keydown', handleKeyDown);
-        document.addEventListener('keydown', handleKeyUp);
-        document.getElementById('timertijd').innerText = `${1/timeSpan*1000} Hz (${timeSpan} ms)`;
-        if(highscore === null)
+        document.removeEventListener('keydown', handleKeyDown);
+        document.removeEventListener('keyup', handleKeyUp);
+
+        const currentTime = new Date().getTime(); // Get current time in milliseconds
+
+        if (firstClickTime === null)
         {
-            highscore = timeSpan;
+            firstClickTime = currentTime; // Record the time of the first click
+            console.log('First click recorded. Click again to measure the time span.');
+            document.getElementById('timertijd').innerText = "Klik opnieuw om de timer te stoppen";
         }
         else
         {
-            if(timeSpan < highscore)
+            const timeSpan = currentTime - firstClickTime; // Calculate the time difference
+            console.log(`Time span between clicks: ${timeSpan} ms`);
+            firstClickTime = null; // Reset for the next measurement
+            document.addEventListener('keydown', handleKeyDown);
+            document.addEventListener('keydown', handleKeyUp);
+            document.getElementById('timertijd').innerText = `${1/timeSpan*1000} Hz (${timeSpan} ms)`;
+            if(highscore === null)
             {
                 highscore = timeSpan;
             }
+            else
+            {
+                if(timeSpan < highscore)
+                {
+                    highscore = timeSpan;
+                }
+            }
+            amountOfTries ++;
+            total += timeSpan;
+            if(total >= Number.MAX_SAFE_INTEGER || amountOfTries >= Number.MAX_SAFE_INTEGER)
+            {
+                amountOfTries = 1;
+                total = timeSpan;
+                alert("voortgang verwijderd");
+            }
+            document.getElementById('highscore').innerText = `persoonlijk record: ${1/highscore*1000} Hz (${highscore} ms)`;
+            document.getElementById('average').innerText = `persoonlijk gemiddelde: ${1/(total/amountOfTries)*1000} Hz (${total/amountOfTries}ms)`;
+            modus = "none";
         }
-        amountOfTries ++;
-        total += timeSpan;
-        if(total >= Number.MAX_SAFE_INTEGER || amountOfTries >= Number.MAX_SAFE_INTEGER)
-        {
-            amountOfTries = 1;
-            total = timeSpan;
-            alert("voortgang verwijderd");
-        }
-        document.getElementById('highscore').innerText = `persoonlijk record: ${1/highscore*1000} Hz (${highscore} ms)`;
-        document.getElementById('average').innerText = `persoonlijk gemiddelde: ${1/(total/amountOfTries)*1000} Hz (${total/amountOfTries}ms)`;
     }
 }
 
 const handleKeyDown = () =>
 {
-    document.removeEventListener('keydown', handleKeyDown);
-    document.addEventListener('keyup', handleKeyUp);
-    let knop = document.getElementById('knop');
-    knop.disabled = true;
-    //document.removeEventListener('click', handleClick);
-
-    const currentTime = new Date().getTime(); // Get current time in milliseconds
-
-    if (firstClickTime === null)
+    if(modus === "none")
     {
-        firstClickTime = currentTime; // Record the time of the first click
-        console.log('First click recorded. Click again to measure the time span.');
-        document.getElementById('timertijd').innerText = "Klik opnieuw om de timer te stoppen";
+        modus = "key";
     }
-    else
+    if(modus === "key")
     {
-        const timeSpan = currentTime - firstClickTime; // Calculate the time difference
-        console.log(`Time span between clicks: ${timeSpan} ms`);
-        firstClickTime = null; // Reset for the next measurement
-        knop.disabled = false;
-        document.getElementById('timertijd').innerText = `${1/timeSpan*1000} Hz (${timeSpan} ms)`;
-        if(highscore === null)
+        document.removeEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+        let knop = document.getElementById('knop');
+        knop.disabled = true;
+        //document.removeEventListener('click', handleClick);
+
+        const currentTime = new Date().getTime(); // Get current time in milliseconds
+
+        if (firstClickTime === null)
         {
-            highscore = timeSpan;
+            firstClickTime = currentTime; // Record the time of the first click
+            console.log('First click recorded. Click again to measure the time span.');
+            document.getElementById('timertijd').innerText = "Klik opnieuw om de timer te stoppen";
         }
         else
         {
-            if(timeSpan < highscore)
+            const timeSpan = currentTime - firstClickTime; // Calculate the time difference
+            console.log(`Time span between clicks: ${timeSpan} ms`);
+            firstClickTime = null; // Reset for the next measurement
+            knop.disabled = false;
+            document.getElementById('timertijd').innerText = `${1/timeSpan*1000} Hz (${timeSpan} ms)`;
+            document.getElementById('average').innerText = `persoonlijk gemiddelde: ${1/(total/amountOfTries)*1000} Hz (${total/amountOfTries}ms)`;
+            if(highscore === null)
             {
                 highscore = timeSpan;
             }
+            else
+            {
+                if(timeSpan < highscore)
+                {
+                    highscore = timeSpan;
+                }
+            }
+            amountOfTries ++;
+            total += timeSpan;
+            if(total >= Number.MAX_SAFE_INTEGER || amountOfTries >= Number.MAX_SAFE_INTEGER)
+            {
+                amountOfTries = 1;
+                total = timeSpan;
+                alert("voortgang verwijderd");
+            }
+            document.getElementById('highscore').innerText = `persoonlijk record: ${1/highscore*1000} Hz (${highscore} ms)`;
+            modus = "none";
         }
-        document.getElementById('highscore').innerText = `persoonlijk record: ${1/highscore*1000} Hz (${highscore} ms)`;
     }
 }
 
 
 const handleKeyUp = () =>
 {
-    document.addEventListener('keydown', handleKeyDown);
-    document.removeEventListener('keyup', handleKeyUp);
+    if(modus === "key")
+    {
+        document.addEventListener('keydown', handleKeyDown);
+        document.removeEventListener('keyup', handleKeyUp);
+    }
 }
 
 window.addEventListener('load', handleLoad);
