e465bc0e84a2df7186761e6d36402556f8298043
MattijsVandebroekPXL <mattijs.vandebroek@student.pxl.be>
2025-12-20 11:42:53 +0100

    wordle kerstraadsel geüpdated
    nieuwe naam ingesteld
    alles ziet er nu ook mooier uit
    wordlist toegevoegd

--- website/overige/kerstraadsel/kerstraadsel.js
+++ website/overige/kerstraadsel/kerstraadsel.js
@@ -1,21 +1,39 @@
 'use strict'
 
-const encoded = "bGV2ZW50ZQ=="
+const encoded = "bWFtYQ=="
 const secretWord = atob(encoded).toLowerCase(); // hardcoded naam (max 7)
 
 const board = document.getElementById("board");
 const input = document.getElementById("guessInput");
 const button = document.getElementById("guessBtn");
 
-const MAX_LEN = 7;
+const MAX_LEN = 20;
+const wordList = ["luk", "pup", "maarten", "papa", "griet", "mama", "hannes", "lukas", "mattijs", "lander", "heleen"];
+
+const handleKeyDown = (e) =>
+{
+    if (e.key === "Enter")
+    {
+        checkGuess();
+    }
+}
 
 button.addEventListener("click", checkGuess);
+document.addEventListener("keydown", handleKeyDown);
 
-function checkGuess() {
+function checkGuess()
+{
     const guess = input.value.toLowerCase();
 
-    if (guess.length === 0 || guess.length > 7) {
-        alert("Ik denk niet dat er iemand mee doet met een naam langer dan 7 letters.");
+    if (guess.length === 0 || guess.length > 20)
+    {
+        alert("De naam is te lang of te kort.");
+        return;
+    }
+
+    if(!wordList.includes(guess.toLowerCase()))
+    {
+        alert("Not in wordlist.");
         return;
     }
 
